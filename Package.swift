54d6fc69dafa4c46e6ac7a7bcde0dd4e33b3b9fa
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-10-26 19:33:46 +0100

    fix #100

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
         .library(name: "AWSLambdaEvents", targets: ["AWSLambdaEvents"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-http-types.git", exact: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.5.1")
     ],
     targets: [
         .target(
--- Package@swift-5.10.swift
+++ Package@swift-5.10.swift
@@ -11,8 +11,7 @@ let package = Package(
         .library(name: "AWSLambdaEvents", targets: ["AWSLambdaEvents"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-http-types.git", from: "1.5.1")
     ],
     targets: [
         .target(
